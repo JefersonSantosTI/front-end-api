9e4916eaed02a419fdf88b3bed2544cd58d3b184
JEFERSON TI <jefersonbrandaosantos@gmail.com>
2026-03-11 00:01:09 -0300

    Refactor Mensagem component to remove loading prop

--- src/components/Mensagem.jsx
+++ src/components/Mensagem.jsx
@@ -1,26 +1,24 @@
+const Mensagem = ({ mensagem }) => {
 
+    const isBot = mensagem.remetente === "bot"
 
-const Mensagem = ({ mensagem, loading }) => {
-    const isBot = mensagem.remetente === 'bot';
-    {
-        loading && isBot && (
-            <span className="text-xs text-gray-400">Digitando...</span>
-        )
-    }
     return (
-        <div className={`flex ${isBot ? 'justify-start' : 'justify-end'}`}>
-            <div className={`max-w-xs lg:max-w-md px-5 py-4 rounded-2xl shadow-2xl hover:shadow-xl cursor-pointer
-                ${isBot ? 'bg-gray-50 text-gray-800 rounded-bl-none border border-gray-300'
+        <div className={`flex ${isBot ? "justify-start" : "justify-end"}`}>
 
-
-                    : 'bg-linear-to-r from-purple-300 to-emerald-400 text-white rounded-br-none'}
-                
-                
-                `}>
-                <p className="text-sm whitespace-pre-line">{mensagem.texto}</p>
+            <div
+                className={`max-w-[85%] sm:max-w-md px-4 sm:px-5 py-3 rounded-2xl shadow
+          ${isBot
+                        ? "bg-white text-gray-800 border border-gray-200 rounded-bl-none"
+                        : "bg-emerald-500 text-white rounded-br-none"
+                    }`}
+            >
+                <p className="text-sm sm:text-base whitespace-pre-line">
+                    {mensagem.texto}
+                </p>
             </div>
-        </div>
-    );
-};
 
-export default Mensagem;
+        </div>
+    )
+}
+
+export default Mensagem
