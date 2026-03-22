04b1f05292a90f9c67af61e43bfd25adaf480a6a
JefersonSantosTI <jefersonbrandaosantos@gmail.com>
2026-03-22 14:54:16 -0300

    ajuste final nas variaveis e conexao mongodb

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://api-Backend-treino-fit.onrender.com"
+  baseURL: "https://api-backend-treino-fit.onrender.com"
 })
