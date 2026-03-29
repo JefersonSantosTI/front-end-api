616fc821da5c4ec93d9199e6d671d052947364ff
JefersonSantosTI <jefersonbrandaosantos@gmail.com>
2026-03-29 13:57:18 -0300

    assets: adiciona ícones oficiais do PWA

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'logo192.png'],
+      manifest: {
+        name: 'Treino Fit - Consultoria IA',
+        short_name: 'Treino Fit',
+        description: 'Sua dieta e treino personalizados com IA',
+        theme_color: '#10b981', // Verde esmeralda do seu botão
+        background_color: '#0f172a', // Cor escura do seu fundo
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
- 
 })
